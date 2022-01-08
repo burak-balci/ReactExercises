6e13ddbaebb8deca7ccb502c68a5f0dd50a98b31
slnxA <burakb199726@gmail.com>
2022-01-08 16:03:38 +0300

    Homework is done.

--- IdeaProjects/Hipotenus/src/Main.java
+++ IdeaProjects/Hipotenus/src/Main.java
@@ -3,18 +3,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double a,b,c,u,alan;
-        Scanner girdi = new Scanner(System.in);
-        System.out.print("1. Kenarı Giriniz : ");
-        a = girdi.nextInt();
-        System.out.print("2. Kenarı Giriniz : ");
-        b = girdi.nextInt();
-        System.out.print("3. Kenarı Giriniz : ");
-        c = girdi.nextDouble();
-        u = (a+b+c) /2;
-        System.out.println("Üçgenin Çevresi : " + 2*u);
-        alan = Math.sqrt(u*(u-a)*(u-b)*(u-c));
-        System.out.print("Üçgenin Alanı : " + alan);
+
+        double a,alan,r,pi = 3.14;
+        System.out.print("Dairenin Yarıçapını Giriniz : ");
+        Scanner input = new Scanner(System.in);
+        r = input.nextInt();
+        System.out.print("Dairenin Merkez Açısını giriniz : ");
+        a = input.nextDouble();
+        alan = (pi*r* r * a )/360;
+        System.out.print("Daire Diliminin Alanı : " + alan);
 
     }
 }
